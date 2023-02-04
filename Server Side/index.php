2aff1790c5fd8328a5245aec7e62039d5297483e
admin <purnesh.xyz@gmail.com>
2023-02-04 10:43:02 +0000

    AS

--- Server Side/index.php
+++ Server Side/index.php
@@ -8,6 +8,7 @@ $url = "http://www.quora.com/How-can-we-make-a-Chrome-Extension-using-data-scrap
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $curl_scraped_page = curl_exec($ch);
+
 curl_close($ch);
 echo $curl_scraped_page;
 
